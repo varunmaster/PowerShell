864e45befae2dbc4356bd936c64ae4f2d2dfeca2
Varun Master <varunmaster95@gmail.com>
2018-04-29 13:45:43 -0400

    Removed password for ftp

--- MoveMoviestoServer_Plex1.ps1
+++ MoveMoviestoServer_Plex1.ps1
@@ -1,35 +1,28 @@
 ﻿#Directory where files are located
-$FromDir= Get-ChildItem "C:\Users\vm305\Desktop\moviesToUpload" -Recurse
-$FromDir1= Get-ChildItem "C:\Users\vm305\Desktop\moviesToUpload" -Recurse
+$FromDir = Get-ChildItem "C:\Users\vm305\Desktop\moviesToUpload" -Recurse
+$toDir = Get-Item \\VARUNPLEX-PC\Users\Varun\Desktop\incomingMovies\*
+
 
 #ftp server info
 $ftp = "ftp://Varun@192.168.1.179/"
 $user = "Varun"
-$pass = "Drqjrk2hnhbg9ngt"
+$pass = ""
 
 $webclient = New-Object System.Net.WebClient
 $webclient.Credentials = New-Object System.Net.NetworkCredential($user,$pass)
 
 
-#Pick up files
+#Pick up files and push
 foreach($item in $FromDir){
-    "Uploading $item..."
-    $uri = New-Object System.Uri($ftp+$item.Name)
-    $webclient.UploadFile($uri, $item.FullName)
+    if ($($item.Name) -in $($toDir.Name)){
+        #Write-Host "Skipping $item because already on server"
+        continue
+    }else{
+        #"Uploading $item..."
+        $uri = New-Object System.Uri($ftp+$item.Name)
+        $webclient.UploadFile($uri, $item.FullName)
+    }
 }
 
-#foreach($item in $FromDir){
-#    Remove-Item -Path $item.FullName -Recurse
-#}
 
-
-$FromDir1| Remove-Item -Recurse
-
-<#
-#Download files - Below code works but I need to use a wildcard in the file name as it changes and I can't figure out how to do that.
-
-$target = "pc name\C$\FTP\Download\Notes.txt"
-$source =  "<ftp address>/Sample_file.txt"
-
-$WebClient.DownloadFile($source, $target)
-#>
+$FromDir | Remove-Item -Recurse
