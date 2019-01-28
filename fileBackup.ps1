6b01eabd283347acef15703660c91a1f262b39ec
varunmaster <varunmaster95@gmail.com>
2019-01-27 22:32:48 -0500

    MD5 hashfunction
    Changed the hashing function to use MD5. updated the loop for backing up
    .
    .
    TODO: first if statement not working. check logs to confirm

--- fileBackup.ps1
+++ fileBackup.ps1
@@ -1,9 +1,9 @@
 ﻿param(
-    [string]$dirsToScan = "C:\Users\vmaster\Desktop\Test", #default value when param not specified
-    [string]$backupDir = "C:\Users\vmaster\Desktop\Test2"  #default value when param not specified
+    [string]$dirsToScan = "C:\Users\vm305\Desktop\Test", #default value when param not specified
+    [string]$backupDir = "C:\Users\vm305\Desktop\Test2"  #default value when param not specified
 )
 
-$Logfile = "C:\Users\vmaster\Desktop\$($MyInvocation.MyCommand.Name).log"
+$Logfile = "C:\Users\vm305\Desktop\$($MyInvocation.MyCommand.Name).log"
 #$dirsToScan = "C:\Users\vmaster\Desktop\Test"
 #$backupDir = "C:\Users\vmaster\Desktop\Test2"
 $filesToBackup = @()
@@ -24,14 +24,13 @@ function LogWrite($logString)
 
 #hash function to scan the backup and add them to the hashtable
 function getFileHash($fileName){
-    $index = -1
-    $nameToCharArr = [int[]][char[]]$fileName
-    $sum = 0
-    for ($i = 0; $i -lt $nameToCharArr.length-1; $i++){
-        $sum += $nameToCharArr[$i]
+    $hasher = New-Object System.Security.Cryptography.MD5CryptoServiceProvider
+    $toHash = [System.Text.Encoding]::UTF8.GetBytes($fileName)
+    $hashByteArray = $hasher.ComputeHash($toHash)
+    foreach($byte in $hashByteArray){
+        $result += "{0:X2}" -f $byte
     }
-    $index = $sum % 350 #change the mod
-    return $index
+    return $result;
 }
 
 
@@ -67,22 +66,37 @@ foreach($dir in ((Get-ChildItem $dirsToScan -Directory).FullName)) {
 #else skip
 foreach($file in $filesToBackup){
     #splitting it so we can compare just the file names and not the directory since they will be different
-    if ($filesInBackup.ContainsValue($file.Split("\")[-1]) -and `
-        (($file.Length -ne (Get-Item $filesInBackup.ContainsValue($file.Split("\")[-1])).Length) -or ($file.LastWriteTime -ne (Get-Item $filesInBackup.ContainsValue($file.Split("\")[-1])).LastWriteTime))){
+    if ($filesInBackup.ContainsKey((getFileHash $file.Split("\")[-1])) -and `
+        ((Get-Item $file).Length -ne ((Get-Item($filesInBackup.Item((getFileHash $file.Split("\")[-1]))).FullName).Length) -or `
+            (Get-Item $file).LastWriteTime -ne ((Get-Item($filesInBackup.Item((getFileHash $file.Split("\")[-1]))).FullName).LastWriteTime))){
         Copy-Item $file -Destination $backupDir
         LogWrite((Get-Date).toString("yyyy/MM/dd HH:mm:ss") + ": Copying item to backup since it was updated: " + $file.Split("\")[-1])
     }
-    elseif($file.Split("\")[-1] -notin $filesInBackup.ContainsValue($file.Split("\")[-1])){
+    elseif($file.Split("\")[-1] -notin ($filesInBackup.Item((getFileHash $file.Split("\")[-1])))){
         Copy-Item $file -Destination $backupDir
-        LogWrite((Get-Date).toString("yyyy/MM/dd HH:mm:ss") + ": Copying item to backup since not currently backed up: " + $file.Split("\")[-1])
+        LogWrite((Get-Date).toString("yyyy/MM/dd HH:mm:ss") + ": Copying item to backup; not latest copy: " + $file.Split("\")[-1])
     }
     else{
         continue
-        LogWrite((Get-Date).toString("yyyy/MM/dd HH:mm:ss") + ": Skipping item since already backed up or no changes were made: " + $file.Split("\")[-1])
+        LogWrite((Get-Date).toString("yyyy/MM/dd HH:mm:ss") + ": Skipping item since already backed up and no changes were made: " + $file.Split("\")[-1])
     }
 }
 
-$filesToBackup.Clear()
-$filesInBackup.Clear()
+#$filesToBackup.Clear()
+#$filesInBackup.Clear()
 LogWrite((Get-Date).toString("yyyy/MM/dd HH:mm:ss") + ": Script ended")
 LogWrite("------------------------------------End------------------------------------")
+
+
+<# old hash function that is very crappy
+function getFileHash($fileName){
+    $index = -1
+    $nameToCharArr = [int[]][char[]]$fileName
+    $sum = 0
+    for ($i = 0; $i -lt $nameToCharArr.length-1; $i++){
+        $sum += $nameToCharArr[$i]
+    }
+    $index = $sum % 350 #change the mod
+    return $index
+}
+#>
