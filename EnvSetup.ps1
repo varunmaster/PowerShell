a87b39d048d37ad522fa2aa062d3545aa926d36b
Varun Master <varunmaster95@gmail.com>
2019-05-13 18:02:44 -0400

    adding script to set up VM

---
+++ EnvSetup.ps1
@@ -0,0 +1,47 @@
+param(
+    [Parameter(Mandatory = $true)][String] $hostName,
+    [Parameter(Mandatory = $false)][String] $ip,
+    [Parameter(Mandatory = $true)][String] $DNS,
+    [Parameter(Mandatory = $true)][String] $ipMode,
+    [Parameter(Mandatory = $true)][String] $firewallRuleName,
+    [Parameter(Mandatory = $true)][String] $firewallRulePort
+)
+
+$Logfile = "C:\Logs\$($MyInvocation.MyCommand.Name).log"
+function LogWrite($logString)
+{
+   Add-content $Logfile -value $logString 
+}
+
+function changeIP ($ip) {
+    try {
+        $currName = hostname
+        $ipOld = Test-Connection -ComputerName $currName -Count 1 | Select-Object IPV4Address
+
+        if ($ipOld -ne $ip) {
+            New-NetIPAddress -IPAddress $ip -DefaultGateway 192.168.1.1 -PrefixLength 24
+        }else {
+            LogWrite("Current IP is: $($ipOld) and new IP is : $($ip). Nothing changed")
+        }
+    }
+    catch{
+        LogWrite($_.Exception.Message)
+    }
+}
+
+function changeDNS ($DNS){
+    try{
+        Set-DnsClientServerAddress -ServerAddresses 192.168.1.166, 8.8.8.8
+    }
+    catch{
+        LogWrite($_.Exception.Message)
+    }
+}
+
+function firewallRules($name, $port){
+    New-NetFirewallRule -DisplayName $name -Direction Inbound -LocalPort $port -Protocol TCP -Action Allow
+}
+
+function renameComputer ($newName){
+    Rename-Computer -NewName $hostName
+}
