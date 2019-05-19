22f5f45941afde7a25dfbdd5c17890ca157d677c
varunmaster <varunmaster95@gmail.com>
2019-05-19 15:56:06 -0400

    updated envsetup and removed envsetup2.0

--- EnvSetup.ps1
+++ EnvSetup.ps1
@@ -1,7 +1,7 @@
 param(
     [Parameter(Mandatory = $false,HelpMessage="What you want to rename comp to")][String] $hostName,
     [Parameter(Mandatory = $false,HelpMessage="What you want ip to be")][String] $ip,
-    [Parameter(Mandatory = $false,HelpMessage="Repoint to 192.168.1.166")] $DNS,
+    [Parameter(Mandatory = $false,HelpMessage="Repoint to 192.168.1.166")][Switch] $DNS,
 #    [Parameter(Mandatory = $true)][String] $ipMode,
     [Parameter(Mandatory = $false,HelpMessage="Name of rule, e.g. Allow My Rules")][String] $firewallRuleName,
     [Parameter(Mandatory = $false,HelpMessage="The port numbers: 25,587,32400,8080,8085")] $firewallRulePort
@@ -19,10 +19,11 @@ function changeIP ($ip) {
         $ipOld = Test-Connection -ComputerName $currName -Count 1 | Select-Object IPV4Address
 
         if ($ipOld -ne $ip) {
-            LogWrite("IP changed")
-            #New-NetIPAddress -IPAddress $ip -InterfaceAlias 'Ethernet0' -PrefixLength 24 -DefaultGateway 192.168.1.1
+            #Write-Host "IP changed"
+            Remove-NetIPAddress -IPAddress $ipOld -InterfaceAlias Ethernet0 -AddressFamily IPv4 -PrefixLength 24 -DefaultGateway 192.168.1.1
+            New-NetIPAddress -IPAddress $ip -InterfaceAlias 'Ethernet0' -PrefixLength 24 -DefaultGateway 192.168.1.1
         }else {
-            LogWrite("Current IP is: $($ipOld) and new IP is : $($ip). Nothing changed")
+            Write-Host "Current IP is: $($ipOld) and new IP is : $($ip). Nothing changed"
         }
     }
     catch{
@@ -32,13 +33,8 @@ function changeIP ($ip) {
 
 function changeDNS (){
     try{
-        if (!$DNS){
-            LogWrite("DNS changed")
-            #Set-DnsClientServerAddress -ServerAddresses 192.168.1.166, 8.8.8.8 
-        }
-        else {
-            Write-Host "enter 'pihole' as the DNS parameter"
-        }
+        #Write-Host "DNS changed"
+        Set-DnsClientServerAddress -ServerAddresses ("192.168.1.166", "8.8.8.8") -InterfaceAlias Ethernet0
     }
     catch{
         LogWrite($_.Exception.Message)
@@ -47,8 +43,8 @@ function changeDNS (){
 
 function firewallRules($name, $port){
     try{
-        LogWrite("firewall rule changed")
-        #New-NetFirewallRule -DisplayName $name -Direction Inbound -LocalPort $port -Protocol TCP -Action Allow -Group 
+        #Write-Host "firewall rule changed"
+        New-NetFirewallRule -DisplayName $name -Direction Inbound -LocalPort $port -Protocol TCP -Action Allow -Group 
     }
     catch{
         LogWrite($_.Exception.Message)
@@ -57,32 +53,15 @@ function firewallRules($name, $port){
 
 function renameComputer ($newName){
     try{
-        LogWrite("computer name changed")
-        #Rename-Computer -NewName $hostName 
+        #Write-Host "computer name changed"
+        Rename-Computer -NewName $hostName 
     }
     catch{
         LogWrite($_.Exception.Message)
     }
 }
 
-if(!$ip){
-    changeIP -ip $ip
-}else{
-    continue
-}
-if(!$DNS){
-    changeDNS
-}else{
-    continue
-}
-if(!$firewallRuleName){
-    firewallRules -name $firewallRuleName -port $firewallRulePort
-}else{
-    continue
-}
-if (!$firewallRulePort) {
-    renameComputer -newName $hostName 
-}else{
-    continue
-}
-Read-Host "tea;lst"
+changeIP -ip $ip
+changeDNS
+firewallRules -name $firewallRuleName -port $firewallRulePort
+renameComputer -newName $hostName 
