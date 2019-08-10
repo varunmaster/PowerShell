f272724c16814c7d7fdc9c89ee14ddc09a9fd14f
varunmaster <varunmaster95@gmail.com>
2019-08-10 18:35:17 -0400

    initial commit

---
+++ SendLatestInventory.ps1
@@ -0,0 +1,5 @@
+﻿function getLatestMovies($prevDays){
+    gci -Path 'C:\Data\Movies' -Directory | ? {$_.CreationTime.ToString("MM/dd/yyyy") -le ((Get-Date).AddDays(-$prevDays).ToString("MM/dd/yyyy"))}
+}
+
+Invoke-RestMethod -Method Get 
