0957a1b457ff36d82443c7fc0c9b789fa5e6a10a
Varun <varunmaster95@gmail.com>
2019-08-11 00:52:47 -0400

    basic stuff

--- SendLatestInventory.ps1
+++ SendLatestInventory.ps1
@@ -2,4 +2,7 @@
     gci -Path 'C:\Data\Movies' -Directory | ? {$_.CreationTime.ToString("MM/dd/yyyy") -le ((Get-Date).AddDays(-$prevDays).ToString("MM/dd/yyyy"))}
 }
 
-Invoke-RestMethod -Method Get 
+$token = Get-Content "C:\Users\vm305\Desktop\Scripts1\token.txt"
+$url = "http://www.omdbapi.com/?apikey=$($token)&"
+
+Invoke-RestMethod -Method Get -Uri $url+"t="$name+"&y="$year
