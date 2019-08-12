d43ab991f68c0396400e187f1eec71f4abcdf5e3
Varun <varunmaster95@gmail.com>
2019-08-12 15:12:28 -0400

    finished script. Testing is remaining
    TODO: change the location of token and other variables from MAC to Windows

--- SendLatestInventory.ps1
+++ SendLatestInventory.ps1
@@ -1,8 +1,35 @@
-﻿function getLatestMovies($prevDays){
-    gci -Path 'C:\Data\Movies' -Directory | ? {$_.CreationTime.ToString("MM/dd/yyyy") -le ((Get-Date).AddDays(-$prevDays).ToString("MM/dd/yyyy"))}
+﻿function getName ($movie){
+    $name = ([regex]::Matches("$movie.Name",'[a-zA-Z]+\s\d?')).Value[0] #wholeword,space,optional number
+    return $name
 }
 
-$token = Get-Content "C:\Users\vm305\Desktop\Scripts1\token.txt"
-$url = "http://www.omdbapi.com/?apikey=$($token)&"
+function getYear ($movie){
+    $year = ([regex]::Matches("$movie.Name",'([0-9]{4})')).Value
+    return $year
+}
 
-Invoke-RestMethod -Method Get -Uri $url+"t="$name+"&y="$year
+$movieList = @(gci -Path 'C:\Data\Movies' -Directory | ? {$_.CreationTime.ToString("MM/dd/yyyy") -le ((Get-Date).AddDays(-$prevDays).ToString("MM/dd/yyyy"))}).Name
+$token = Get-Content "/Users/varun/Desktop/token.txt"
+$url = "http://www.omdbapi.com/?apikey=$($token)&"
+$movieListEmail = "<h1>Movies:</h1><br/><br/>"
+$movieListEmail += "<table style=`"width:100%`">"
+$movieCnt=0
+
+foreach($movie in $movieList){
+    $movieCnt++
+    if($movieCnt -gt 1 -and $movieCnt%5 -eq 0){
+        Start-Sleep -Seconds 30
+    }else {
+        $result = Invoke-RestMethod -Method Get -Uri $url+"t="getName($movie)+"&y="getYear($movie)
+        $movieListEmail += "<tr><td><img src=`"$($result.Poster)`"</td>"
+        $movieListEmail += "<td><li>$($result.Title) ($($result.Year))</li>"
+        $movieListEmail += "<td><li><i>Released:</i> $($result.Released)</li>"
+        $movieListEmail += "<td><li><i>Runtime:</i> $($result.Runtime)</li>"
+        $movieListEmail += "<td><li><i>Actors:</i> $($result.Actors)</li>"
+        $movieListEmail += "<td><li><i>Plot:</i> $($result.Plot)</li>"
+    }
+}
+
+$movieListEmail += "</tr></table><br/><br/>"
+
+Send-MailMessage -SMTPServer '###' -To @('###') -From '###' -Subject "Library Updates this week" -Body $movieListEmail -BodyAsHtml
