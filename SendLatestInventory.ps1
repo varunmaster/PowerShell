1756a03554576975ebb0d64a43277e3c752e5626
Varun <varunmaster95@gmail.com>
2019-08-12 22:38:42 -0400

    cleaned up the code

--- SendLatestInventory.ps1
+++ SendLatestInventory.ps1
@@ -1,10 +1,16 @@
-﻿function getName ($movie){
-    $name = ([regex]::Matches("$movie.Name",'[a-zA-Z]+\s\d?')).Value[0] #wholeword,space,optional number
+﻿$Logfile = "C:\Logs\$($MyInvocation.MyCommand.Name).log"
+function LogWrite($logString)
+{
+   Add-content $Logfile -value ((Get-Date).toString("yyyy/MM/dd HH:mm:ss") + ": $logString ")
+}
+
+function getName($movie){
+    $name = ([regex]::Matches("$movie",'[a-zA-Z]+\s\d?')).Value[0] #wholeword,space,optional number
     return $name
 }
 
-function getYear ($movie){
-    $year = ([regex]::Matches("$movie.Name",'([0-9]{4})')).Value
+function getYear($movie){
+    $year = ([regex]::Matches("$movie",'([0-9]{4})')).Value
     return $year
 }
 
@@ -18,18 +24,27 @@ $movieCnt=0
 foreach($movie in $movieList){
     $movieCnt++
     if($movieCnt -gt 1 -and $movieCnt%5 -eq 0){
+        LogWrite("Already sent 5 API requests...Sleeping for 30 seconds")
         Start-Sleep -Seconds 30
     }else {
-        $result = Invoke-RestMethod -Method Get -Uri $url+"t="getName($movie)+"&y="getYear($movie)
-        $movieListEmail += "<tr><td><img src=`"$($result.Poster)`"</td>"
-        $movieListEmail += "<td><li>$($result.Title) ($($result.Year))</li>"
-        $movieListEmail += "<td><li><i>Released:</i> $($result.Released)</li>"
-        $movieListEmail += "<td><li><i>Runtime:</i> $($result.Runtime)</li>"
-        $movieListEmail += "<td><li><i>Actors:</i> $($result.Actors)</li>"
-        $movieListEmail += "<td><li><i>Plot:</i> $($result.Plot)</li>"
+        $result = Invoke-RestMethod -Method Get -Uri $url+"t="+getName($movie)+"&y="+getYear($movie)
+        LogWrite("Sending API GET") 
+        if($result.Length -lt 1){
+            LogWrite("Could not get info...trying next movie")
+            continue
+        }else {
+            LogWrite("Got response...retrieving info")
+            $movieListEmail += "<tr><td><img src=`"$($result.Poster)`"</td>"
+            $movieListEmail += "<td><li>$($result.Title) ($($result.Year))</li>"
+            $movieListEmail += "<td><li><i>Released:</i> $($result.Released)</li>"
+            $movieListEmail += "<td><li><i>Runtime:</i> $($result.Runtime)</li>"
+            $movieListEmail += "<td><li><i>Actors:</i> $($result.Actors)</li>"
+            $movieListEmail += "<td><li><i>Plot:</i> $($result.Plot)</li>"
+        }
     }
 }
 
 $movieListEmail += "</tr></table><br/><br/>"
 
 Send-MailMessage -SMTPServer '###' -To @('###') -From '###' -Subject "Library Updates this week" -Body $movieListEmail -BodyAsHtml
+LogWrite("EMAIL SENT")
