7098a66cc60b329e4a656f4e10b6fdd3577d3f2f
varunmaster <varunmaster95@gmail.com>
2019-10-13 12:49:15 -0400

    added write-host and try/catch inside functions

--- BackItUp.ps1
+++ BackItUp.ps1
@@ -7,34 +7,69 @@ $LogFile = (Join-Path "C:\Logs\" -ChildPath "$($MyInvocation.MyCommand.Name).log
 function LogWrite($logString)
 {
    Add-content $Logfile -value ((Get-Date).toString("yyyy/MM/dd HH:mm:ss") + ": $logString ")
+   Write-Host $logString
 }
 LogWrite("<-----------------------------------Start----------------------------------->")
 
 function backupMovies(){
     $moviesToBackup = (gci T:\Movies\ | ? {$_.LastWriteTime -gt (Get-Date).AddDays(-14)} ).FullName
-    foreach ($movie in $moviesToBackup){
-        Copy-Item -Path $movie -Destination "E:\Movies\" -Force -Recurse
-        LogWrite("Copied Movie <$($movie)> to E:\Movies\")
+    Try{
+        foreach ($movie in $moviesToBackup){
+            Copy-Item -Path $movie -Destination "E:\Movies\" -Force -Recurse
+            LogWrite("Copied Movie <$($movie)> to E:\Movies\")
+        }
+    }
+    Catch{
+        LogWrite("ERROR OCCURRED: $_.Exception.Message")
     }
 }
 
 function backupScripts(){
     $scriptsToBackup = (gci C:\Users\vm305\Desktop\Scripts1 -Recurse).FullName 
-    foreach ($script in $scriptsToBackup){
-        Copy-Item -Path $script -Destination "T:\Files\Scripts1\" -Force -Recurse
-        LogWrite("Copied Script <$($script)> to T:\Files\Scripts1\")
-        Copy-Item -Path $script -Destination "E:\Files\Scripts1\" -Force -Recurse
-        LogWrite("Copied Script <$($script)> to E:\Files\Scripts1")
+    Try{
+        foreach ($script in $scriptsToBackup){
+            Copy-Item -Path $script -Destination "T:\Files\Scripts1\" -Force -Recurse
+            LogWrite("Copied Script <$($script)> to T:\Files\Scripts1\")
+            Copy-Item -Path $script -Destination "E:\Files\Scripts1\" -Force -Recurse
+            LogWrite("Copied Script <$($script)> to E:\Files\Scripts1")
+        }
+    }
+    Catch{
+        LogWrite("ERROR OCCURRED: $_.Exception.Message")
     }
 }
 
 function backupGitHub(){
-    $GitHubBackup = (gci C:\Users\vm305\Documents\GitHub -Directory -Recurse| ?{$_.LastWriteTime -gt (Get-Date).AddDays(-14)}).FullName
-    foreach($item in $GitHubBackup){
-        Copy-Item $item -Destination "T:\Files\GitHub\" -Force -Recurse
-        LogWrite("Copied Script <$($item)> to T:\Files\Github\")
-        Copy-Item -Path $item -Destination "E:\Files\GitHub\" -Force -Recurse
-        LogWrite("Copied Script <$($item)> to E:\Files\GitHub\")
+    $GitHubBackup = (gci C:\Users\vm305\Documents\GitHub -Directory -Recurse | ? {$_.LastWriteTime -gt (Get-Date).AddDays(-14)}).FullName
+    Try{
+        foreach($item in $GitHubBackup){
+            Copy-Item $item -Destination "T:\Files\GitHub\" -Force -Recurse
+            LogWrite("Copied Script <$($item)> to T:\Files\Github\")
+            Copy-Item -Path $item -Destination "E:\Files\GitHub\" -Force -Recurse
+            LogWrite("Copied Script <$($item)> to E:\Files\GitHub\")
+        }
+    }
+    Catch{
+        LogWrite("ERROR OCCURRED: $_.Exception.Message")
+    }
+}
+
+function backupMiscFiles(){
+    $iso = (gci D:\ISOs -Recurse -File).FullName
+    $kee = (gci D:\KeePass -Recurse).FullName
+    Try{
+        foreach($item in $iso){
+            Copy-Item $item -Destination "T:\Files\ISOs\" -Force -Recurse
+            LogWrite("Copied Script <$($item)> to T:\Files\ISOs\")
+        }
+        foreach($item in $kee){
+            Copy-Item $item -Destination "T:\Files\KeePass" -Force -Recurse
+            LogWrite("Copied Script <$($item)> to T:\Files\KeePass")
+        }
+        
+    }
+    Catch{
+        LogWrite("ERROR OCCURRED: $_.Exception.Message")
     }
 }
 
@@ -44,6 +79,9 @@ LogWrite("Going to start backing up Scripts")
 backupScripts
 LogWrite("Going to start backing up GitHub")
 backupGitHub
+LogWrite("Going to start backing up Misc files")
+backupMiscFiles
+
 $stopwatch.Stop()
 LogWrite("Total time: <$($stopwatch.Elapsed.TotalSeconds)>")
 LogWrite("<------------------------------------End------------------------------------>")
